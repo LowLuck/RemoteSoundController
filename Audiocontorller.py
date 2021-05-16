70ffef3f8e81858f3b4c761353eff3f2e446a8c4
LowLuck <cfif905@bk.ru>
2021-05-16 15:52:30 +0300

    Functional upgrade Typo error in process name fix

--- Audiocontorller.py
+++ Audiocontorller.py
@@ -2,7 +2,7 @@ from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 from audiocontrollerclass import AudioController
 
 
-def main():
+def starter():
     sessions = AudioUtilities.GetAllSessions()
     s = []
     s2 = []
@@ -20,36 +20,65 @@ def sys_clear(lis):
     return s
 
 
-if __name__ == "__main__":
-    q = main()
+def refresh(start=True):
+    global q, p
+    q = starter()
     p = sys_clear(q[1])
-    print('Processes in work')
-    print(p)
-    print('Choose a process to work')
-    w = input()
+    if start:
+        print(p)
+    return [j.Process.name() for j in q[0] if j.Process]
+
+
+def main(w):
     audio_controller = AudioController(w)
     print(f'\n'
           f'WARNING! Values are percentages of master volume! \n'
           f'1 -- sound equals to master, 0.5 half of main etc. \n'
           f'up <number> -- increase <percent> \n'
           f'down <number> --  decrease <percent> \n'
+          f'half -- half of volume \n'
+          f'full -- max volume \n'
           f'mute -- mute \n'
           f'unmute -- unmute \n'
           f'stop -- stop the program')
     while True:
-        w = input()
+        w = input().split()
         if w == 'stop':
             break
-        elif w[:2] == 'up':
-            num = w[3:]
+        elif w[0] == 'up':
+            num = w[1]
             audio_controller.increase_volume(float(num))
-        elif w[:4] == 'down':
-            num = w[5:]
+        elif w[0] == 'down':
+            num = w[1]
             audio_controller.decrease_volume(float(num))
-        elif w[:4] == 'mute':
+        elif w[0] == 'half':
+            audio_controller.set_volume(0.5)
+        elif w[0] == 'full':
+            audio_controller.set_volume(1)
+        elif w[0] == 'mute':
             audio_controller.mute()
-        elif w[:6] == 'unmute':
+        elif w[0] == 'unmute':
             audio_controller.unmute()
+
         else:
             print('Command not found')
     print('Exit')
+    return
+
+
+if __name__ == "__main__":
+    q = None
+    p = None
+    namelist = refresh(False)
+    print('Processes in work (type refresh to refresh)')
+    print(p)
+    print('Choose a process to work')
+
+    while True:
+        w = input()
+        if w in namelist:
+            main(w)
+            break
+        else:
+            print('Process not found')
+            namelist = refresh()
